5477e0a2417a73dfd2f6363a70e9bc627ae26d1b
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-01-12 14:31:05 +0000

    Inserted API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateFact(event) {
-  event.preventDefault();
-
+function displayFact(response) {
+  console.log("fact generated");
   new Typewriter("#fact", {
-    strings: "Generating a fact",
+    strings: response.data.answer,
     autoStart: true,
     delay: 2,
     cursor: "",
   });
+}
+
+function generateFact(event) {
+  event.preventDefault();
+
+  let feedInput = document.querySelector;
+  let apiKey = "97ca3fd7c1847b8c2t8cf2o00a730a3a";
+  let prompt = `Generate an interesting fact about ${feedInput.value}`;
+  let context =
+    "You are a knowledgeable AI assistant that can provide interesting facts.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  console.log("Finding fact");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayFact);
 }
 
 let factFormElement = document.querySelector("#fact-finder-form");
