1def326e344f06850f501df0ab973d4548832937
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-01-12 16:02:44 +0000

    Upgraded the CSS

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateFact(event) {
   let topicInput = document.querySelector("#user-topic");
   let apiKey = "97ca3fd7c1847b8c2t8cf2o00a730a3a";
   let context =
-    "You are a knowledgeable AI assistant that can provide intriguing short facts. Your misson is to provide a fact in basic HTML, which is no longer than 8 lines. Do not include a title. Make sure to follow user instructions.";
+    "You are a knowledgeable AI assistant that can provide intriguing short facts. Your misson is to provide a fact in basic HTML, which is no longer than 8 lines. Do not include a title or heading. Make sure to follow user instructions.";
   let prompt = `User instructions: Generate an interesting fact about ${topicInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
