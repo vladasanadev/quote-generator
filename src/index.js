136cd173451a853980bdd58270e642576d95b64f
EmiliaPodmore <148763142+EmiliaPodmore@users.noreply.github.com>
2024-01-13 14:15:06 +0000

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -14,9 +14,13 @@ function generateFact(event) {
   let topicInput = document.querySelector("#user-topic");
   let apiKey = "97ca3fd7c1847b8c2t8cf2o00a730a3a";
   let context =
-    "You are a knowledgeable AI assistant that can provide intriguing short facts. Your misson is to provide a fact in basic HTML, which is no longer than 8 lines. Do not include a title or heading. Make sure to follow user instructions.";
-  let prompt = `User instructions: Generate an interesting fact about ${topicInput.value}`;
+    "You are a knowledgeable AI assistant that can provide intriguing short facts. Your mission is to present a fact using basic HTML, keeping it within 6 lines. Avoid including a title or heading, and ensure adherence to user instructions.";
+  let prompt = `User instructions: Generate a short interesting fact about ${topicInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let factElement = document.querySelector("#fact");
+  factElement.classList.remove("hidden");
+  factElement.innerHTML = `<div class = "blink">Finding a fact about ${topicInput.value} for you</div>`;
 
   console.log("Finding fact");
   console.log(`Prompt: ${prompt}`);
